37b37de805a62f247644a727bb918adeb10c88b7
prahladinala <prahladinala@users.noreply.github.com>
2020-09-02 23:47:54 +0530

    Uploaded Files
    Uploaded Files

---
+++ notworking.js
@@ -0,0 +1,37 @@
+// Listen for Submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+//Calculate Result
+function calculateResults(e){
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    //Formulaes to calculate
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value)/12;
+
+    //Compute the monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+
+    //Check if "monthly" number is finite or note
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly*calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+    }else{
+        console.log("Please check")
+    }
+
+    //Calculations
+    
+
+    e.preventDefault();
+}
